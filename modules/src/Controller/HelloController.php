50577ff51fad9cea55911f1179d89b3d2d53ec58
Steve Rudolfi <sr4136@gmail.com>
2026-03-13 15:30:47 -0500

    add custom block template, custom css; modify controller to output classes

--- modules/src/Controller/HelloController.php
+++ modules/src/Controller/HelloController.php
@@ -1,48 +1,41 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\hello_world\Controller\HelloController.
- */
-
 namespace Drupal\hello_world\Controller;
 
 use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\Session\SessionManagerInterface;
-
-
 
 class HelloController {
-	/* Gets the current user's name. */
-	public function getCurrentUserName() {
+    /* Gets the current user's name. */
+    public function getCurrentUserName() {
+        $current_user = \Drupal::currentUser();
+        $user = \Drupal\user\Entity\User::load($current_user->id());
 
-		// Get the current user service.
-		$current_user = \Drupal::currentUser();
+        return $user instanceof AccountInterface ? $user->getDisplayName() : 'Anonymous';
+    }
 
-		// Load the user entity to access more information.
-		$user = \Drupal\user\Entity\User::load($current_user->id());
+    /* Output the content. */
+    public function content($context = 'controller') {
+        $name = $this->getCurrentUserName();
 
-		// Get the user's name (full name).
-		if ($user instanceof AccountInterface) {
-			$name = $user->getDisplayName(); // Gets the display name.
-		} else {
-			$name = 'Anonymous';
-		}
+        $output_structure = 'From all of us at [@context context]: HELLO WORLD, and specifically <strong><i>@name</i></strong>!';
+        $output_vars = ['@name' => $name, '@context' => $context];
+        $output_translated = t($output_structure, $output_vars);
 
-		return $name;
-	}
-
-	/* Output the content. */
-	public function content($context = 'controller') {
-		$name = $this->getCurrentUserName(); // Get the user's name.
-
-		$output_structure = 'From all of us at [@context context]: HELLO WORLD, and specifically <strong><i>@name</i></strong>!';
-		$output_vars = ['@name' => $name, '@context' => $context];
-		$output_translated = t($output_structure, $output_vars);
-
-		return array(
-			'#type' => 'markup',
-			'#markup' => $output_translated,
-		);
-	}
+        // Update the render array to match block output
+        return [
+            '#type' => 'container',
+            '#attributes' => [
+                'class' => [
+                    'contextual-region',
+                    'block-stever-helloworld',
+                    'stever-helloworld',
+                    'context-'.$context
+                ],
+            ],
+            'content' => [
+                '#type' => 'markup',
+                '#markup' => $output_translated,
+            ],
+        ];
+    }
 }
